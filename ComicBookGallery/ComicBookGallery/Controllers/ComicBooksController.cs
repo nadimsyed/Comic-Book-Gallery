27fdd19efcb7c576468d2d9a10161093e324b427
Nadim <syed.m.nadim@gmail.com>
2018-05-20 16:01:25 -0400

    Added Comic Book Detail view

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,17 +11,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-                //return new RedirectResult("/");
-            }
-
-            return Content("Hello from the comic books controller");
-            //return new ContentResult()
-            //{
-            //    Content = "Hello from the comic books controller"
-            //};
+            return View();
         }
     }
 }
